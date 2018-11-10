58406419780f7fdbb7bb6d16ff624f8e09dfe5f6
Komang.Ariana <km.ariana1992@gmail.com>
2018-11-10 15:40:56 +0800

    Create component hero banner and assign it to page home.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,46 @@
 <template>
   <main>
+    <section class="above-content">
+      <app-hero-banner
+        :title="heroTitle"
+        :description="heroDescription"
+        :image="heroImage"
+        :link="heroLink"
+      >
+      </app-hero-banner>
+    </section>
+    <section class="content container">
+    </section>
   </main>
 </template>
 
 <script>
+import axios from 'axios'
+import AppHeroBanner from '@/components/AppHeroBanner'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    AppHeroBanner
+  },
+  data () {
+    return {
+      heroTitle: 'Welcome to DANA',
+      heroDescription: '',
+      heroImage: 'https://source.unsplash.com/random',
+      heroLink: '',
+      heroData: Object
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.chucknorris.io/jokes/random')
+      .then(response => {
+        this.heroData = response
+        this.heroDescription = this.heroData.data.value
+        this.heroLink = this.heroData.data.url
+      })
+  }
 }
 </script>
 
