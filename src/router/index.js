267c86b5e66affdeda8946923f58b4f893e3cdf0
Komang.Ariana <km.ariana1992@gmail.com>
2018-11-12 22:10:57 +0800

    Create and style contact us page.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
+import ContactUs from '@/components/ContactUs'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
       path: '/webtask/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/webtask/contact',
+      name: 'ContactUs',
+      component: ContactUs
     }
   ]
 })
